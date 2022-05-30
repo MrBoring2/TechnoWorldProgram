789048103ae5c9cabfacc653ecd27cda53c8b63e
MrBoringHome <denis_simonov1337@mail.ru>
2022-05-30 14:27:10 +0500

    dd

--- TechnoWorld_WarehouseAccounting/ViewModels/Windows/LoginWindowVM.cs
+++ TechnoWorld_WarehouseAccounting/ViewModels/Windows/LoginWindowVM.cs
@@ -1,29 +1,12 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using TechnoWorld_WarehouseAccounting.Services;
-using Microsoft.AspNetCore.SignalR.Client;
-using RestSharp;
 using Newtonsoft.Json;
-using TechnoWorld_WarehouseAccounting.Models;
-using System.Windows;
-using System.Security;
-using TechnoWorld_WarehouseAccounting.Views.Windows;
 using WPF_VM_Abstractions;
 using WPF_Helpers.Abstractions;
 using WPF_Helpers.Common;
-using TechoWorld_DataModels_v2;
 using TechoWorld_DataModels_v2.Models;
-using SweetAlertSharp;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Data;
 using MaterialNotificationLibrary;
 using MaterialNotificationLibrary.Enums;
-using System.Security.Cryptography;
 using WPF_Helpers.Services;
 
 namespace TechnoWorld_WarehouseAccounting.ViewModels.Windows
@@ -46,50 +29,42 @@ namespace TechnoWorld_WarehouseAccounting.ViewModels.Windows
             LoginCommand = new RelayCommand(Authorize);
         }
         /// <summary>
-        /// Авторизация
+        /// Метод авторизации пользователя в системе
+        /// Здесь происходит получение токена доступа от сервера
         /// </summary>
         /// <param name="obj"></param>
         private async void Authorize(object obj)
         {
-            try
-            {
-                if (Login.Length >= 30 || Password.Length >= 30)
-                {
-
-                    return;
-                }
-
-                IsEnabled = false;
-
-
-
-                var response = await ApiService.Instance.AuthorizeInWarehouse(Login, MD5EncoderService.EncodePassword(Login, Password));
-                if (response.StatusCode == System.Net.HttpStatusCode.OK)
-                {
-                    var data = JsonConvert.DeserializeObject<AuthResponseModel>(response.Content);
-                    ClientService.Instance.SetClient(data.user_name, data.full_name, data.role_id, data.user_id, data.post);
-
-                    await ApiService.Instance.GetHubConnection.StartAsync();
-
-                    WindowNavigation.Instance.OpenAndHideWindow(this, new MainAppWindowVM());
-
-                }
-                else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
-                {
-                    MaterialNotification.Show("Ошибка", JsonConvert.DeserializeObject<string>(response.Content), MaterialNotificationButton.Ok, MaterialNotificationImage.Error);
-                    IsEnabled = true;
-                }
-                else
-                {
-                    MaterialNotification.Show("Критическая ошибка.", "Потярено соединение с сервером!", MaterialNotificationButton.Ok, MaterialNotificationImage.Error);
-                    IsEnabled = true;
-                }
-
-            }
-            catch (Exception ex)
+            if (Login.Length >= 30 || Password.Length >= 30)
             {
 
+                return;
             }
+
+            IsEnabled = false;
+
+            var response = await ApiService.Instance.AuthorizeInWarehouse(Login, MD5EncoderService.EncodePassword(Login, Password));
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var data = JsonConvert.DeserializeObject<AuthResponseModel>(response.Content);
+                ClientService.Instance.SetClient(data.user_name, data.full_name, data.role_id, data.user_id, data.post);
+
+                await ApiService.Instance.GetHubConnection.StartAsync();
+
+                WindowNavigation.Instance.OpenAndHideWindow(this, new MainAppWindowVM());
+
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                MaterialNotification.Show("Ошибка", JsonConvert.DeserializeObject<string>(response.Content), MaterialNotificationButton.Ok, MaterialNotificationImage.Error);
+                IsEnabled = true;
+            }
+            else
+            {
+                MaterialNotification.Show("Критическая ошибка.", "Потярено соединение с сервером!", MaterialNotificationButton.Ok, MaterialNotificationImage.Error);
+                IsEnabled = true;
+            }
+
         }
     }
 }
